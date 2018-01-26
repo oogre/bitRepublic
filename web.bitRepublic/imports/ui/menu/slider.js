cab8d6a6b2b75c5288304e5f96cf2a39821bad49
acr <hello@crucifixarnaud.com>
2018-01-26 15:22:36 +0100

    Hero-banner

--- web.bitRepublic/imports/ui/menu/slider.js
+++ web.bitRepublic/imports/ui/menu/slider.js
@@ -5,22 +5,24 @@ export default class SliderMenu extends Component {
 	constructor(props){
 		super(props);
 	}
-	
+
 	render() {
-	    return (
-			<div className="container">
-				<h2>
-					Make the data economy benefits all
-				</h2>
-				<ul>
-					<li>
-						<a href="#">Design your taxbot</a>
-					</li>
-					<li>
-						<a href="#">Find out more</a>
-					</li>
-				</ul>
+		return (
+			<div className="hero-banner">
+				<div className="container">
+					<div className="hero-banner__content">
+						<h2 className="hero-banner__title">Make the data economy benefits all</h2>
+						<ul className="buttons-list">
+							<li className="buttons-list__item">
+								<a className="button button--md hero-banner__button" href="#">Design your taxbot</a>
+							</li>
+							<li className="buttons-list__item">
+								<a className="button button--md hero-banner__button" href="#">Find out more</a>
+							</li>
+						</ul>
+					</div>
+				</div>
 			</div>
 		);
-  	}
+	}
 }
