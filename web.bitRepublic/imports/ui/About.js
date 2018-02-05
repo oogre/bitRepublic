2faf5bf879d120bd16acf3406e4d638eb7216aa8
vincent <vincent@ogre.be>
2018-02-05 18:05:45 +0100

    footer + a bit of about

--- web.bitRepublic/imports/ui/About.js
+++ web.bitRepublic/imports/ui/About.js
@@ -2,13 +2,14 @@
   bitRepublic - About.js
   @author Evrard Vincent (vincent@ogre.be)
   @Date:   2018-01-30 21:21:06
-  @Last Modified time: 2018-02-02 00:08:34
+  @Last Modified time: 2018-02-05 18:04:39
 \*----------------------------------------*/
 import React, { Component } from 'react';
 import HeaderMenu from './menu/header.js';
 import SliderMenu from './menu/slider.js';
 import FooterMenu from './menu/footer.js';
-
+import FixePunchline from './fixe/punchline.js';
+import FixeSlider from './fixe/slider.js';
 // App component - represents the whole app
 export default class About extends Component {
 	constructor(props){
@@ -21,6 +22,10 @@ export default class About extends Component {
 				<div className="page__content">
 					<HeaderMenu />
 					<SliderMenu />
+					<FixePunchline description={["design your tax collector bot", "claim a #bitsoiltax"]}>
+						<a className="button--secondary hero-banner__button" href={FlowRouter.path("home") + "#taxbot"}>Create now</a>
+					</FixePunchline>
+					<FixeSlider />
 				</div>
 				<FooterMenu />
 			</div>
--- web.bitRepublic/imports/ui/menu/footer.js
+++ web.bitRepublic/imports/ui/menu/footer.js
@@ -2,7 +2,7 @@
   bitRepublic - footer.js
   @author Evrard Vincent (vincent@ogre.be)
   @Date:   2018-01-25 18:10:49
-  @Last Modified time: 2018-02-05 14:11:07
+  @Last Modified time: 2018-02-05 17:28:21
 \*----------------------------------------*/
 import React, { Component } from 'react';
 
@@ -22,13 +22,29 @@ export default class FooterMenu extends Component {
 								<a className="menu__item__link" href={FlowRouter.path("contact")}>Contact</a>
 							</li>
 							<li className="menu__item">
-								<a className="menu__item__link" href="#">Supported by</a>
-							</li>
-							<li className="menu__item">
-								<a className="menu__item__link" href="#">Partners</a>
+								<a className="menu__item__link" href="mailto:info@larbitslab.be">info@larbitslab.be</a>
 							</li>
 							<li className="menu__item">
 								<p className="menu__item__text">Licensed under Creative Commons</p>
+							</li>
+							<li className="menu__item">
+								<p className="menu__item__text">SUPPORTED BY</p>
+								<ul className="menu menu--footer">
+									<li className="menu__item">
+										<p className="menu__item__text">Flemish community</p>
+									</li>
+									<li className="menu__item">
+										<p className="menu__item__text">XXXXXXXXXXXXXX</p>
+									</li>
+								</ul>
+							</li>
+							<li className="menu__item">
+								<p className="menu__item__text">Partners</p>
+								<ul className="menu menu--footer">
+									<li className="menu__item">
+										<a className="menu__item__link" href="mailto:info@larbitslab.be">XXXXXXXXXXXXXX</a>
+									</li>
+								</ul>
 							</li>
 						</ul>
 					</nav>
