09185bf0acca4ee1aba52db0a78f7ef27358f5f1
vincent <vincent@ogre.be>
2018-04-06 19:47:24 +0200

    remove shell command to reset bistoil to consume

--- web.bitRepublic/imports/api/bitsoils/bitsoils.js
+++ web.bitRepublic/imports/api/bitsoils/bitsoils.js
@@ -2,12 +2,11 @@
   bitRepublic - bitsoils.js
   @author Evrard Vincent (vincent@ogre.be)
   @Date:   2018-01-25 16:09:47
-  @Last Modified time: 2018-04-06 19:21:45
+  @Last Modified time: 2018-04-06 19:46:15
 \*----------------------------------------*/
 import { Mongo } from 'meteor/mongo';
 
 import './restAPI.js';
 import './methods.js';
-import './commands.js';
 
 export const Bitsoils = null;
